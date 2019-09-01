7413a045116d387679d64941d12eaf7a021655ea
Tomasz Markowski <tomekmarkowski92@gmail.com>
2019-09-01 11:01:53 +0200

    Dodano wybór kategorii. Zmieniono system listy kategorii.

--- src/main/java/QuizMain.java
+++ src/main/java/QuizMain.java
@@ -1,26 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class QuizMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        String[] listaKategorii = new String[16];
-        listaKategorii[0] = "C:/Test/quiz/Animals.txt";
-        listaKategorii[1] = "C:/Test/quiz/Entertainment_Board_Games.txt";
-        listaKategorii[2] = "C:/Test/quiz/Entertainment_Books.txt";
-        listaKategorii[3] = "C:/Test/quiz/Entertainment_Cartoon__Animations.txt";
-        listaKategorii[4] = "C:/Test/quiz/Entertainment_Film.txt";
-        listaKategorii[5] = "C:/Test/quiz/Entertainment_Japanese_Anime__Manga.txt";
-        listaKategorii[6] = "C:/Test/quiz/Entertainment_Music.txt";
-        listaKategorii[7] = "C:/Test/quiz/Entertainment_Television.txt";
-        listaKategorii[8] = "C:/Test/quiz/Entertainment_Video_Games.txt";
-        listaKategorii[9] = "C:/Test/quiz/General_Knowledge.txt";
-        listaKategorii[10] = "C:/Test/quiz/Geography.txt";
-        listaKategorii[11] = "C:/Test/quiz/History.txt";
-        listaKategorii[12] = "C:/Test/quiz/Science__Nature.txt";
-        listaKategorii[13] = "C:/Test/quiz/Science_Computers.txt";
-        listaKategorii[14] = "C:/Test/quiz/Sports.txt";
-        listaKategorii[15] = "C:/Test/quiz/Vehicles.txt";
 
-        int wybranaWybranaKategoria;
-        //...
+        File folder = new File("C:/Test/quiz/");
+        File[] listaKategorii = folder.listFiles();
+
+        int wybranaKategoria = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wybierz kategorię: ");
+        System.out.println("1.Animals\n2.Board Games\n3.Books\n4.Cartoon Animations\n5.Film\n6.Anime\n7.Music\n8.Television" +
+                "\n9.Video games\n10.General knowledge\n11.Geography\n12.History\n13.Science - Computers\n14.Science - Nature" +
+                "\n15.Sports\n16.Vehicles");
+        wybranaKategoria = scanner.nextInt();
+
+        Scanner scanner2 = new Scanner(listaKategorii[wybranaKategoria-1]);
+
+        while (scanner2.hasNextLine()){
+            String text = scanner2.nextLine();
+            System.out.println(text);
+        }
+
+
+
+
 
 
 
